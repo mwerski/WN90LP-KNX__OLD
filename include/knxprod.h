64a315bb77f837b14d03d193860781f06caeeffe
Michael Werski <michael@werski.de>
2025-06-05 20:25:23 +0200

    basic knxprod

--- include/knxprod.h
+++ include/knxprod.h
@@ -25,7 +25,25 @@
 #define APP_Heartbeat_Mask	0x0FFF
 // Offset: 1, Size: 12 Bit, Text: "In Betrieb" senden alle
 #define ParamAPP_Heartbeat ((uint32_t)((knx.paramWord(APP_Heartbeat) >> APP_Heartbeat_Shift) & APP_Heartbeat_Mask))
+#define APP_Date/Time_DPTs		0x0000
+// Offset: 0, BitOffset: 5, Size: 1 Bit, Text: Empfangen über
+#define ParamAPP_Date/Time_DPTs knx.paramBit(APP_Date/Time_DPTs, 5)
+#define APP_Watchdog		0x0000
+// Offset: 0, BitOffset: 6, Size: 1 Bit, Text: Watchdog aktivieren
+#define ParamAPP_Watchdog knx.paramBit(APP_Watchdog, 6)
 //!< Number: 1, Text: In Betrieb, Function: Statusmeldung
 #define APP_KoHeartbeat 1
 #define KoAPP_Heartbeat knx.getGroupObject(APP_KoHeartbeat)
+//!< Number: 0, Text: Fehlermeldung, Function: 
+#define APP_KoFehlermeldung 0
+#define KoAPP_Fehlermeldung knx.getGroupObject(APP_KoFehlermeldung)
+//!< Number: 0, Text: Zeit, Function: Eingang
+#define APP_KoTime 0
+#define KoAPP_Time knx.getGroupObject(APP_KoTime)
+//!< Number: 0, Text: Datum, Function: Eingang
+#define APP_KoDate 0
+#define KoAPP_Date knx.getGroupObject(APP_KoDate)
+//!< Number: 0, Text: Datum/Zeit, Function: Eingang
+#define APP_KoDate/Time 0
+#define KoAPP_Date/Time knx.getGroupObject(APP_KoDate/Time)
 
