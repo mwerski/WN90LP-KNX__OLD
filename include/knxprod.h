f848f4230bf5f6c24eb09dd2c7b4400517986d63
Michael Werski <michael@werski.de>
2025-06-05 20:42:50 +0200

    extended knxprod

--- include/knxprod.h
+++ include/knxprod.h
@@ -11,7 +11,7 @@
 #define MAIN_ApplicationNumber 0x00
 #define MAIN_ApplicationVersion 0x01
 #define MAIN_OrderNumber "p3-00012.1"
-#define MAIN_ParameterSize 3
+#define MAIN_ParameterSize 4
 #define MAIN_MaxKoNumber 1
 
 
@@ -26,11 +26,20 @@
 // Offset: 1, Size: 12 Bit, Text: "In Betrieb" senden alle
 #define ParamAPP_Heartbeat ((uint32_t)((knx.paramWord(APP_Heartbeat) >> APP_Heartbeat_Shift) & APP_Heartbeat_Mask))
 #define APP_Date/Time_DPTs		0x0000
-// Offset: 0, BitOffset: 5, Size: 1 Bit, Text: Empfangen über
+// Offset: 0, BitOffset: 5, Size: 1 Bit, Text: Zeit empfangen über
 #define ParamAPP_Date/Time_DPTs knx.paramBit(APP_Date/Time_DPTs, 5)
 #define APP_Watchdog		0x0000
 // Offset: 0, BitOffset: 6, Size: 1 Bit, Text: Watchdog aktivieren
 #define ParamAPP_Watchdog knx.paramBit(APP_Watchdog, 6)
+#define APP_1wire_vorhanden?		0x0000
+// Offset: 0, BitOffset: 7, Size: 1 Bit, Text: 1wire Sensor vorhanden
+#define ParamAPP_1wire_vorhanden? knx.paramBit(APP_1wire_vorhanden?, 7)
+#define APP_Feinstaubsensor_vorhanden?		0x0003
+// Offset: 3, Size: 1 Bit, Text: Feinstaubsensor vorhanden
+#define ParamAPP_Feinstaubsensor_vorhanden? knx.paramBit(APP_Feinstaubsensor_vorhanden?, 0)
+#define APP_Uhrzeit_beim_Start_lesen?		0x0003
+// Offset: 3, BitOffset: 1, Size: 1 Bit, Text: Uhrzeit beim Start lesen
+#define ParamAPP_Uhrzeit_beim_Start_lesen? knx.paramBit(APP_Uhrzeit_beim_Start_lesen?, 1)
 //!< Number: 1, Text: In Betrieb, Function: Statusmeldung
 #define APP_KoHeartbeat 1
 #define KoAPP_Heartbeat knx.getGroupObject(APP_KoHeartbeat)
