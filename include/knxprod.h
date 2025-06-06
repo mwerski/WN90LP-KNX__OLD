ef8af05fa989fabb4f743416481e1f3902079de1
Michael Werski <michael@werski.de>
2025-06-06 09:03:37 +0200

    removed Umlauts from knxprod

--- include/knxprod.h
+++ include/knxprod.h
@@ -11,7 +11,7 @@
 #define MAIN_ApplicationNumber 0x00
 #define MAIN_ApplicationVersion 0x01
 #define MAIN_OrderNumber "p3-00012.1"
-#define MAIN_ParameterSize 16
+#define MAIN_ParameterSize 146
 #define MAIN_MaxKoNumber 7
 
 
@@ -37,37 +37,55 @@
 #define APP_Feinstaubsensor_vorhanden		0x0003
 // Offset: 3, Size: 1 Bit, Text: Feinstaubsensor
 #define ParamAPP_Feinstaubsensor_vorhanden knx.paramBit(APP_Feinstaubsensor_vorhanden, 0)
+#define APP_MQTT_Host		0x0004
+// Offset: 4, Size: 256 Bit (32 Byte), Text: MQTT Broker Hostname/IP
+#define ParamAPP_MQTT_Host knx.paramData(APP_MQTT_Host)
+#define APP_MQTT_Port		0x0024
+// Offset: 36, Size: 16 Bit (2 Byte), Text: MQTT Broker Port
+#define ParamAPP_MQTT_Port ((uint32_t)((knx.paramWord(APP_MQTT_Port))))
+#define APP_MQTT_User		0x0026
+// Offset: 38, Size: 256 Bit (32 Byte), Text: MQTT Broker Username
+#define ParamAPP_MQTT_User knx.paramData(APP_MQTT_User)
+#define APP_MQTT_Password		0x0046
+// Offset: 70, Size: 256 Bit (32 Byte), Text: MQTT Broker Password
+#define ParamAPP_MQTT_Password knx.paramData(APP_MQTT_Password)
+#define APP_MQTT_Topic		0x0066
+// Offset: 102, Size: 256 Bit (32 Byte), Text: MQTT Topic
+#define ParamAPP_MQTT_Topic knx.paramData(APP_MQTT_Topic)
 #define APP_Uhrzeit_beim_Start_lesen		0x0003
 // Offset: 3, BitOffset: 1, Size: 1 Bit, Text: Uhrzeit beim Start lesen
 #define ParamAPP_Uhrzeit_beim_Start_lesen knx.paramBit(APP_Uhrzeit_beim_Start_lesen, 1)
 #define APP_Temperatur_DPT		0x0003
 // Offset: 3, BitOffset: 2, Size: 1 Bit, Text: Sende Temperatur als
 #define ParamAPP_Temperatur_DPT knx.paramBit(APP_Temperatur_DPT, 2)
-#define APP_Temperatur_Senden_zyklisch		0x0004
+#define APP_Temperatur_Senden_zyklisch		0x0086
 #define APP_Temperatur_Senden_zyklisch_Shift	4
 #define APP_Temperatur_Senden_zyklisch_Mask	0x0FFF
-// Offset: 4, Size: 12 Bit, Text: Sende Temperatur alle
+// Offset: 134, Size: 12 Bit, Text: Sende Temperatur alle
 #define ParamAPP_Temperatur_Senden_zyklisch ((uint32_t)((knx.paramWord(APP_Temperatur_Senden_zyklisch) >> APP_Temperatur_Senden_zyklisch_Shift) & APP_Temperatur_Senden_zyklisch_Mask))
-#define APP_Temperatur_Senden_Wertänderung_absolut		0x0006
-// Offset: 6, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
-#define ParamAPP_Temperatur_Senden_Wertänderung_absolut knx.paramFloat(APP_Temperatur_Senden_Wertänderung_absolut, Float_Enc_DPT9)
-#define APP_Temperatur_Senden_Wertänderung_relativ		0x0008
-// Offset: 8, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
-#define ParamAPP_Temperatur_Senden_Wertänderung_relativ knx.paramFloat(APP_Temperatur_Senden_Wertänderung_relativ, Float_Enc_DPT9)
+#define APP_Temperatur_Senden_Wertaenderung_absolut		0x0088
+// Offset: 136, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
+#define ParamAPP_Temperatur_Senden_Wertaenderung_absolut knx.paramFloat(APP_Temperatur_Senden_Wertaenderung_absolut, Float_Enc_DPT9)
+#define APP_Temperatur_Senden_Wertaenderung_relativ		0x008A
+// Offset: 138, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
+#define ParamAPP_Temperatur_Senden_Wertaenderung_relativ knx.paramFloat(APP_Temperatur_Senden_Wertaenderung_relativ, Float_Enc_DPT9)
 #define APP_Feuchte_DPT		0x0003
 // Offset: 3, BitOffset: 3, Size: 1 Bit, Text: Sende Feuchte als
 #define ParamAPP_Feuchte_DPT knx.paramBit(APP_Feuchte_DPT, 3)
-#define APP_Feuchte_Senden_zyklisch		0x000A
+#define APP_Feuchte_Senden_zyklisch		0x008C
 #define APP_Feuchte_Senden_zyklisch_Shift	4
 #define APP_Feuchte_Senden_zyklisch_Mask	0x0FFF
-// Offset: 10, Size: 12 Bit, Text: Sende relative Feuchte alle
+// Offset: 140, Size: 12 Bit, Text: Sende relative Feuchte alle
 #define ParamAPP_Feuchte_Senden_zyklisch ((uint32_t)((knx.paramWord(APP_Feuchte_Senden_zyklisch) >> APP_Feuchte_Senden_zyklisch_Shift) & APP_Feuchte_Senden_zyklisch_Mask))
-#define APP_Feuchte_Senden_Wertänderung_absolut		0x000C
-// Offset: 12, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
-#define ParamAPP_Feuchte_Senden_Wertänderung_absolut knx.paramFloat(APP_Feuchte_Senden_Wertänderung_absolut, Float_Enc_DPT9)
-#define APP_Feuchte_Senden_Wertänderung_relativ		0x000E
-// Offset: 14, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
-#define ParamAPP_Feuchte_Senden_Wertänderung_relativ knx.paramFloat(APP_Feuchte_Senden_Wertänderung_relativ, Float_Enc_DPT9)
+#define APP_Feuchte_Senden_Wertaenderung_absolut		0x008E
+// Offset: 142, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
+#define ParamAPP_Feuchte_Senden_Wertaenderung_absolut knx.paramFloat(APP_Feuchte_Senden_Wertaenderung_absolut, Float_Enc_DPT9)
+#define APP_Feuchte_Senden_Wertaenderung_relativ		0x0090
+// Offset: 144, Size: 16 Bit (2 Byte), Text: Sende bei Änderung um
+#define ParamAPP_Feuchte_Senden_Wertaenderung_relativ knx.paramFloat(APP_Feuchte_Senden_Wertaenderung_relativ, Float_Enc_DPT9)
+#define APP_RemoteDebug		0x0003
+// Offset: 3, BitOffset: 4, Size: 1 Bit, Text: RemoteDebug aktivieren
+#define ParamAPP_RemoteDebug knx.paramBit(APP_RemoteDebug, 4)
 //!< Number: 1, Text: In Betrieb, Function: Statusmeldung
 #define APP_KoHeartbeat 1
 #define KoAPP_Heartbeat knx.getGroupObject(APP_KoHeartbeat)
